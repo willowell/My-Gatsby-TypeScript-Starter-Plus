bb62ddc2a97e5ee924d4aa2d8000693fb1d0b417
William Howell <wlm.howell@gmail.com>
2020-01-06 07:11:53 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'code',
-        path: `${__dirname}/src/content`
+        path: `${__dirname}/content`
       }
     },
     {
