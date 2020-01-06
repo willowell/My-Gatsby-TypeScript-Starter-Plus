86b2406be726349dd0b8d642de843abd50410dd0
William Howell <wlm.howell@gmail.com>
2020-01-06 09:48:43 -0500

    Remove primitive css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-import "./src/styles/primitive.css"
 import "prism-themes/themes/prism-atom-dark.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.css"
