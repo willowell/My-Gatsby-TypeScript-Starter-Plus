4054b55ce5738d667c4a559b65f7776c618c91cc
William Howell <wlm.howell@gmail.com>
2020-01-06 07:32:22 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-typescript-plus',
+    title: 'My Gatsby TypeScript Starter Plus',
     description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
     keywords: 'gatsbyjs, gatsby, javascript, sample, something',
     siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
     author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
-      email: 'resir014@gmail.com'
+      name: 'William Howell',
+      url: 'https://github.com/willowell',
+      email: 'wlm.howell@gmail.com'
     }
   },
   pathPrefix: "/My-Gatsby-TypeScript-Starter-Plus",
